340191990655c25d2c93441b424735851a25f1c8
Yannic Ahrens <yannic@ofmlabs.org>
2012-06-07 17:34:57 +0200

    Adding the (commented) body like a bau5

--- trans.py
+++ trans.py
@@ -8,11 +8,23 @@ if __name__ == '__main__':
     module = __import__(module_name)
     code_buffer = cStringIO.StringIO()
     for class_decl in inspect.getmembers(module, inspect.isclass):
-        class_name = class_decl[0]
+
+        class_name = class_decl[0] 
         l = "%s: class {\n" % (class_name)
-        code_buffer.write(l)
+        code_buffer.write(l) # class name, e.g. "A: class {"
+
+        """
+        # getsourcelines gives back a tuple like
+        # (['class A:\n', '    pass\n'], 1)
+        """
+
+        code_buffer.write("/*\n") # commenting out the python code
+        body = inspect.getsourcelines(class_decl[1])[0]
+        for line in body[1:]: # no need to keep the class header
+            code_buffer.write(line)
+        code_buffer.write("*/\n") # uncommenting
         code_buffer.write("}\n\n")
-        print inspect.getsourcelines(class_decl[1])
+
     ooc_name = "%s.ooc" % module_name
 
 
